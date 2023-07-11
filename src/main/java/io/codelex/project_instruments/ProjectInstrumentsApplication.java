06757aba75a7aed3554317ad8ba8e736426f4c5e
JayH2K <jurgishirss2000@gmail.com>
2023-07-12 00:06:22 +0300

    Continuing setup

---
+++ src/main/java/io/codelex/project_instruments/ProjectInstrumentsApplication.java
@@ -0,0 +1,13 @@
+package io.codelex.project_instruments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectInstrumentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectInstrumentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/codelex/project_instruments/ProjectInstrumentsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.codelex.project_instruments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectInstrumentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
